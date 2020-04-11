5a7cd3b87ecba674de6d6b2b9b7cbd71c519d0bf
Leonardo Pereira <33332673+LeoPereiraVra@users.noreply.github.com>
2020-04-10 21:41:42 -0300

    Exclusão mutua
    Criado exclusão mutua

--- src/bullyelect/BullyElect.java
+++ src/bullyelect/BullyElect.java
@@ -11,6 +11,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
 import java.util.TimerTask;
+import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -23,95 +26,77 @@ public class BullyElect {
      */
     private static Bully bully;
     private static final Timer TIMER = new Timer();
-    private static final int INICIAPROCESSO = 30;
-    private static final int INATIVAPROCESSO = 80;
-    private static final int REQUISICAO = 25;
-    private static final int INATIVACOORDENADOR = 100;
-    
+    private static final int INICIAPROCESSO = 40000;
+    private static final int REQUISICAO = 25000;
+    private static final int INATIVACOORDENADOR = 120000;
+
     public static void main(String[] args) {
-        System.err.println("Sistemas Distribuídos - Bully");
+        System.err.println("Sistemas Distribuídos - Bully - Exclusão mutua");
         System.err.println("Leonardo Pereira - Alexandre Romero");
         bully = new Bully();
-        TIMER.schedule(new CriarProcesso(), 1);
-        TIMER.schedule(new InativarProcesso(), INATIVAPROCESSO * 1000);
-        TIMER.schedule(new RequisitarCoordenador(), REQUISICAO * 1000);
-        TIMER.schedule(new InativarCoordenador(), INATIVACOORDENADOR * 1000); 
 
-    }
-
-    public static class CriarProcesso extends TimerTask {
-
-        /**
-         * Inicializa timer para riar o processo
-         */
-        @Override
-        public void run() {
-            //a cada 30 segundos um novo processo deve ser criado
-            bully.criaProcesso();
-            TIMER.schedule(new CriarProcesso(), INICIAPROCESSO * 1000);
-        }
-    }
-
-    public static class InativarProcesso extends TimerTask {
-
-        /**
-         * Inicializa timer para inativar um processo a cada 80 segundos
-         */
-        @Override
-        public void run() {
-            //a cada 80 segundos um processo da lista de processos fica inativo 
-            Random r = new Random();
-            Processo p = bully.listaProcessos.get(r.nextInt(bully.listaProcessos.size() - 1));
-            p.inativa();
-            System.out.println(new SimpleDateFormat("dd/MM/YYYY HH:mm:ss").format(new Date())+": Processo " + p.getID() + " foi inativado!");
-            TIMER.schedule(new InativarProcesso(), INATIVAPROCESSO * 1000);
-        }
-    }
-
-    public static class RequisitarCoordenador extends TimerTask {
-
-        /**
-         * Inicializa timer para requisitar ao coodenador a cada 25 segundos
-         */
-        @Override
-        public void run() {
-            Random r = new Random();
-            Processo p;
-            ///a cada 25 segundos um processo fazer uma requisição para o coordenador
-            try{
-
-                do {
-
-                    if (bully.listaProcessos.size() == 1) {
-                        p = bully.listaProcessos.get(0);
-                    } else {
-                        int qtd = bully.listaProcessos.size();
-                        int idx = r.nextInt(qtd - 1);
-                        p = bully.listaProcessos.get(idx);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (;;) {
+                    try {
+                        //a cada 30 segundos um novo processo deve ser criado
+                        bully.criaProcesso();
+                        sleep(INICIAPROCESSO);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(BullyElect.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                    if (p.getAtivo()) {
-                        p.requisitarCoordeandor();
-                    }
-                } while ( !p.getAtivo());
-            }catch(NullPointerException e){
-                System.out.println(new SimpleDateFormat("dd/MM/YYYY HH:mm:ss").format(new Date())+": Não encontrou processo!!");
+
+                }
             }
-            TIMER.schedule(new RequisitarCoordenador(), REQUISICAO * 1000);
-        }
-    }
-    
-    public static class InativarCoordenador extends TimerTask {
+        }).start();
 
-        /**
-         * inicializa timer para inativar o coordenador atual
-         */
-        @Override
-        public void run() {   
-            //a cada 100 segundos o coordenador fica inativo
-            bully.getCoodernador().inativa();
-            System.out.println(new SimpleDateFormat("dd/MM/YYYY HH:mm:ss").format(new Date())+": Coordenador " + bully.getCoodernador().getID() + " foi inativado!");
-            TIMER.schedule(new InativarCoordenador(), INATIVACOORDENADOR * 1000);            
-        };
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (;;) {
+                    try {
+                        //a cada 2 min o coordenador fica inativo            
+                        sleep(INATIVACOORDENADOR);
+                        bully.InativarProcesso(bully.getCoodernador());
+                        System.out.println(new SimpleDateFormat("dd/MM/YYYY HH:mm:ss").format(new Date()) + ": Coordenador " + bully.getCoodernador().getID() + " foi inativado!");
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(BullyElect.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (;;) {
+                    try {
+                        //Requisitar coodernador
+                        Random r = new Random();
+                        int tempo = r.nextInt((25 - 10) + 1) + 10;
+                        Processo p;
+                        if (bully.listaProcessos.size() > 0) {
+                            if (bully.listaProcessos.size() == 1) {
+                                p = bully.listaProcessos.get(0);
+                            } else {
+                                int qtd = bully.listaProcessos.size();
+                                int idx = r.nextInt(qtd - 1);
+                                p = bully.listaProcessos.get(idx);
+                            }
+                            if (p.getAtivo()) {
+                                p.requisitarCoordeandor();
+                            }
+                        }
+                        sleep(tempo * 1000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(BullyElect.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                }
+            }
+        }).start();
     }
-    
+
 }
--- src/bullyelect/Processo.java
+++ src/bullyelect/Processo.java
@@ -7,7 +7,11 @@ package bullyelect;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.Random;
 import java.util.Timer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,13 +20,15 @@ import java.util.Timer;
 public class Processo implements Comparable<Processo> {
 
     private boolean ativo;
-    private int ID;
+    private long ID;
     private Timer timer;
     private Bully eleicao;
     private SimpleDateFormat formatter;
     private Processo coodernador;
+    private LinkedList<Processo> filaDeRequisicao;
+    private Thread servico;
 
-    public Processo(int id, Bully eleicao) {
+    public Processo(long id, Bully eleicao) {
         this.ID = id;
         ativo = true;
         this.eleicao = eleicao;
@@ -33,14 +39,37 @@ public class Processo implements Comparable<Processo> {
         //SetCoordenador(this.eleicao.getCoodernador());
         timer = new Timer();
         formatter = new SimpleDateFormat("dd/MM/YYYY HH:mm:ss");
+        filaDeRequisicao = new LinkedList<>();
+
+        servico = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (getAtivo()) {
+                        Thread.sleep(10);
+                        if (filaDeRequisicao.size() > 0) {
+                            Processo p = filaDeRequisicao.pop();
+                            Random r = new Random();
+                            int tempo = r.nextInt((25 - 5) + 1) + 5;
+                            System.out.println(new SimpleDateFormat("dd/MM/YYYY HH:mm:ss").format(new Date()) + ": Imprimindo de " + p.getID() + " em " + tempo + " segundos...");
+                            Thread.sleep(tempo * 1000);
+                            System.out.println("Fim da impressão de " + p.getID() + "...");
+                        }
+                    }
+                } catch (InterruptedException ex) {                    
+                    Logger.getLogger(Processo.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                servico.interrupt();
+            }
+        });        
     }
 
     public void requisitarCoordeandor() {
         ///Ao fazer uam requisição e o coordenador estiver inativo ou não existir, deve iniciar uma nova solicitação
         if ((eleicao.getCoodernador() != null) && (eleicao.getCoodernador().getAtivo())) {
-            eleicao.getCoodernador().Requisicao(ID);
+            eleicao.getCoodernador().Requisicao(this);
         } else {
-            System.out.println(formatter.format(new Date()) +" P: " + ID + " - Coordenador inativo iniciando eleição.");
+            System.out.println(formatter.format(new Date()) + " P: " + ID + " - Coordenador inativo iniciando eleição.");
             eleicao.iniciarEleicao(this);
         }
     }
@@ -51,23 +80,28 @@ public class Processo implements Comparable<Processo> {
         }
     }
 
-    public void Requisicao(int idProc) {
-        System.out.println(formatter.format(new Date())+" P: " + ID + " - Processo " + idProc + " Requisitou ao coordenador.");
+    public void Requisicao(Processo Proc) {
+        filaDeRequisicao.add(Proc);
+        if (!servico.isAlive()){
+            servico.start();
+        }
+        System.out.println(formatter.format(new Date()) + " P: " + ID + " - Processo " + Proc.getID() + " Requisitou ao coordenador.");
     }
 
     public boolean getAtivo() {
         return this.ativo;
     }
 
-    public int getID() {
+    public long getID() {
         return this.ID;
     }
 
     public void inativa() {
-        ativo = false;
+        ativo = false;        
+        filaDeRequisicao.clear();
     }
-    
-    public void SetCoordenador(Processo coo){
+
+    public void SetCoordenador(Processo coo) {
         coodernador = coo;
     }
 
